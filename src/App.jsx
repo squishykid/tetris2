a14cc36dd8374ffdc4d1162028030213e37de17f
Lizzie Siegle <lizzie.siegle@gmail.com>
2026-07-01 10:11:48 -0700

    feat: change page background to pink-900

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Tetris from './components/Tetris.jsx';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+    <div className="min-h-screen bg-pink-900 flex items-center justify-center">
       <Tetris />
     </div>
   );
